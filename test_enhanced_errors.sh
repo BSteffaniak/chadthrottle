3bdb32886e0f0d7a3a5f0acf90961264ee711360
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-11-10 07:30:00 -0500

    Delete all redundant documentation & test scripts

--- test_enhanced_errors.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "=== Starting ChadThrottle with enhanced error logging ==="
-echo "Kernel version: $(uname -r)"
-echo "Running as: $(whoami)"
-echo ""
-sudo RUST_LOG=debug ./target/release/chadthrottle 2>&1 | tee chadthrottle-debug.log
-
